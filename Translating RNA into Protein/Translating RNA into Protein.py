0a60d8f59124e29c2bc61e0602584c7e70c4c021
Haplescent <johnmerritt95@gmail.com>
2019-12-31 21:19:52 -0800

    Translating RNA into Protein

---
+++ Translating RNA into Protein/Translating RNA into Protein.py
@@ -0,0 +1,65 @@
+"""
+Problem
+The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
+
+The RNA codon table dictates the details regarding the encoding of specific codons into the amino acid alphabet.
+
+Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+
+Return: The protein string encoded by s.
+
+Sample Dataset
+AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
+Sample Output
+MAMAPRTEINSTRING
+"""
+
+"""
+Solution found on following website
+https://www.geeksforgeeks.org/dna-protein-python-3/
+"""
+
+from sys import argv
+script, seq = argv
+
+def mrna_to_dna(t):
+    rna_list = []
+    for char in t:
+        if char == "U":
+            rna_list.append("T")
+        elif char == "A":
+            rna_list.append("A")
+        elif char == "G":
+            rna_list.append("G")
+        elif char == "C":
+            rna_list.append("C")
+    return "".join(rna_list)
+
+def translate_dna_to_protein(seq):
+
+	table = {
+		'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+		'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+		'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+		'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+		'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+		'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+		'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+		'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+		'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+		'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+		'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+		'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+		'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+		'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+		'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+		'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+	}
+	protein = ""
+	if len(seq)%3 == 0:
+		for i in range(0, len(seq), 3):
+			codon = seq[i:i + 3]
+			protein += table[codon]
+	return protein
+
+print(translate_dna_to_protein(mrna_to_dna(seq)))
